83c4a349db9c2f870272703e3c010d54aee02ebf
Sudeep Dalal <sudeep.dalal@enjoy.com>
2020-08-15 07:25:29 -0700

    Initial Commit

---
+++ landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  landmarks
+//
+//  Created by Sudeep on 8/15/20.
+//  Copyright © 2020 Sudeep. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
